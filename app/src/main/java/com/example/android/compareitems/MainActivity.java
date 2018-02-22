48ddd54846c557706086ac55cc94e36a969a8f0a
Kondzik <kmaslowiec@gmail.com>
2018-02-22 12:33:29 +0100

    version 0.5.1 XML/Java
    * price TextView added. Some Java added

--- app/src/main/java/com/example/android/compareitems/MainActivity.java
+++ app/src/main/java/com/example/android/compareitems/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.android.compareitems;
 
 import android.app.Activity;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private int picNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,13 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_main);
+    }
+
+    public void biggerPic(View v){
+
+        ImageView pic = (ImageView) findViewById(R.id.image_left);
+        pic.setImageResource(R.drawable.flower_pot_medium);
+
+
     }
 }
